e2b90591bda131873285a4304080e40f86b70c4c
ekatemi <ekatemi@gmail.com>
2022-09-14 09:09:18 +0200

    js

--- script.js
+++ script.js
@@ -1,37 +1,51 @@
 /* Selecting the HTML form and adding a 'submit' event listener */
-
 const form = document.getElementById("contact-us");
-console.log(form);
+const url = form.action;
+
+let fullName = document.getElementById("fullname");
+let email = document.getElementById("email");
+let phone = document.getElementById("phone");
+let message = document.getElementById("message");
+
 form.addEventListener("submit", function (e) {
   // Prevent default behavior:
   e.preventDefault();
-  // Create new FormData object:
-if....
 
-const url = "https://database.deta.sh/v1/a0wwnrex/contactmessages/items";
-const data = {
-    name: document.getElementById("fullname").value,
-};
+  /*   function validateForm() {
+    if (phone.charAt(0) !== "+") {
+      alert("Phone number should start from +!");
+      return false;
+    }
+  }
+  validateForm(); */
 
-const body = { item: data };
-const fetchParams = {
-  method: "POST",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-    "X-API-Key": "a0wwnrex_JeRhBybn5iFYziStv9d2M6Mchd2b4B4H",
-  },
-  body: JSON.stringify(body),
-};
+  const data = {
+    name: fullName.value,
+    email: email.value,
+    phone: phone.value,
+    message: message.value,
+  };
 
-fetch(url, fetchParams)
-  .then((response) => {
-    if (response.ok) return response.json();
-  })
-  .then((json) => {
-    console.log(json);
-  })
-  .catch((error) => {
-    console.error("error");
-  });
-  });
+  const body = { item: data };
+
+  const fetchParams = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "X-API-Key": "a0wwnrex_JeRhBybn5iFYziStv9d2M6Mchd2b4B4H",
+    },
+    body: JSON.stringify(body),
+  };
+
+  fetch(url, fetchParams)
+    .then((response) => {
+      if (response.ok) return response.json();
+    })
+    .then((json) => {
+      console.log(json);
+    })
+    .catch((error) => {
+      console.error("error");
+    });
+});
